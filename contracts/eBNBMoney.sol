1b90ec921775be229aed0fa1e9225fa3ef1b4103
Prabhakaran <m.prabhakar258@gmail.com>
2020-12-19 09:23:18 +0530

    Total supply changed to 30

--- contracts/eBNBMoney.sol
+++ contracts/eBNBMoney.sol
@@ -42,7 +42,8 @@ contract eBNB is  BEP20Token,OwnableUpgradeSafe{
 
     uint256 private constant DECIMALS = 1;
     uint256 private constant MAX_UINT256 = ~uint256(0);
-    uint256 private constant INITIAL_FRAGMENTS_SUPPLY = 25 * 10**5 * 10**DECIMALS;
+    //Total supply changed.
+    uint256 private constant INITIAL_FRAGMENTS_SUPPLY = 30 * 10**5 * 10**DECIMALS;
 
     // TOTAL_GONS is a multiple of INITIAL_FRAGMENTS_SUPPLY so that _gonsPerFragment is an integer.
     // Use the highest value that fits in a uint256 for max granularity.
